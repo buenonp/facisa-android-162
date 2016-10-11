d8b2ead9dfa6b0b4afe302a7a6d6891880ad5575
Volnei Fuzer <volnei.fuzer@gmail.com>
2016-10-11 16:19:00 -0300

    Transicionar da tela de configuração de perfil para a tela de exibição de todos os Apprizs

--- app/src/main/java/com/apprizme/apprizme/ConfigPerfilActivity.java
+++ app/src/main/java/com/apprizme/apprizme/ConfigPerfilActivity.java
@@ -1,13 +1,29 @@
 package com.apprizme.apprizme;
 
+import android.content.Intent;
+import android.support.annotation.IdRes;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class ConfigPerfilActivity extends AppCompatActivity {
+
+    private ImageButton goToApprizs;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_config_perfil);
+
+        goToApprizs = (ImageButton) findViewById(R.id.goToApprizs);
+        goToApprizs.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ConfigPerfilActivity.this, ApprizsActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
