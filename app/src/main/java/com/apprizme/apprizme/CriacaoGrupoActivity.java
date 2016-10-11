28544950f41964414f5d6c505408559cd4ab893d
paulognb <paulonany@gmail.com>
2016-10-11 01:53:35 -0300

    transicao tela Login -> ApprizsGrupo -> CriacaoGrupo

--- app/src/main/java/com/apprizme/apprizme/CriacaoGrupoActivity.java
+++ app/src/main/java/com/apprizme/apprizme/CriacaoGrupoActivity.java
@@ -8,13 +8,20 @@ import android.widget.Button;
 
 public class CriacaoGrupoActivity extends AppCompatActivity {
 
-
+    private Button toApprizeGrupo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_criacao_grupo);
 
-
+        toApprizeGrupo = (Button) findViewById(R.id.criarButton);
+        toApprizeGrupo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(CriacaoGrupoActivity.this,ApprizsGrupoActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
