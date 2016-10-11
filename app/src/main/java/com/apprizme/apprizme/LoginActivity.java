fad085fdbfca206047062a23295d335f148575bf
Volnei Fuzer <volnei.fuzer@gmail.com>
2016-10-10 21:43:18 -0300

    criaçao da tela de mensagem
    (cherry picked from commit cb09432792ddb666d213bb76c80cf0fb74c834e9)
    
    # Conflicts:
    #	.idea/misc.xml
    #	app/src/main/AndroidManifest.xml

--- app/src/main/java/com/apprizme/apprizme/LoginActivity.java
+++ app/src/main/java/com/apprizme/apprizme/LoginActivity.java
@@ -1,13 +1,29 @@
 package com.apprizme.apprizme;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends Activity {
+
+    private ImageButton returnToSignUp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        returnToSignUp = (ImageButton) findViewById(R.id.ImgBTreturnToSignUp);
+        returnToSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this, SignUpActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
     }
 }
--- app/src/main/java/com/apprizme/apprizme/SignUpActivity.java
+++ app/src/main/java/com/apprizme/apprizme/SignUpActivity.java
@@ -1,13 +1,29 @@
 package com.apprizme.apprizme;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
-public class SignUpActivity extends AppCompatActivity {
+public class SignUpActivity extends Activity {
+
+    private TextView goToLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        goToLogin = (TextView) findViewById(R.id.text_new_cadastro);
+        goToLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SignUpActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
     }
 }
