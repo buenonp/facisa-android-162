2f6ed7c51406d25f4bc3fda0fd7049d5c3f9d904
paulognb <paulonany@gmail.com>
2016-10-11 01:44:03 -0300

    transicao tela Login -> PesquisaGrupos -> ApprizsGrupo

--- app/src/main/java/com/apprizme/apprizme/CriacaoGrupoActivity.java
+++ app/src/main/java/com/apprizme/apprizme/CriacaoGrupoActivity.java
@@ -1,13 +1,20 @@
 package com.apprizme.apprizme;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class CriacaoGrupoActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_criacao_grupo);
+
+
     }
 }
