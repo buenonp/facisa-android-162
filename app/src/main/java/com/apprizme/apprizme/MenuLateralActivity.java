42f0316bac2aebb0f684f344fd2d5ecdf4497ab0
Bueno Nepomuceno <buenonp@gmail.com>
2016-10-12 09:19:47 -0300

    Removido conteúdo do frame principal

--- app/src/main/java/com/apprizme/apprizme/MenuLateralActivity.java
+++ app/src/main/java/com/apprizme/apprizme/MenuLateralActivity.java
@@ -21,8 +21,6 @@ import android.widget.Toast;
 public class MenuLateralActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
-    ListView listView;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -48,41 +46,6 @@ public class MenuLateralActivity extends AppCompatActivity
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
 
-        listView = (ListView) findViewById(R.id.list);
-
-        String[] values = new String[] {
-                "Não haverá aula hoje pela manhã",
-                "Alunos com multas atrasadas na biblioteca façam o favor de pagar",
-                "Notas de Desenvolvimento mobile no ar",
-                "Meu telemóvel está a carregar",
-                "Comida liberada na cantina na conta do Professor Daniel Abella",
-                "Não haverá aula na sexta-feira anterior ao fds das eleições",
-                "Não haverá aula na segunda-feira posterior ao fds das eleições",
-                "Disciplina de Android vai dar 10 pra todo mundo",
-                "Segunda, Terça e Quarta da próxima semana é feriadão",
-                "Entreguem os projetos de Android",
-                "Entrega do projeto de Android foi adiada",
-                "Facisa é primeira colocada na olimpíada de programação",
-                "Sistemas de Informação da Facisa é segundo melhor do país",
-                "Empresa canadense busca por profissionais qualificados"
-        };
-
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
-                android.R.layout.simple_list_item_1, android.R.id.text1, values);
-
-        listView.setAdapter(adapter);
-
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-
-                String itemValue = (String) listView.getItemAtPosition(position);
-                Toast.makeText(getApplicationContext(), itemValue, Toast.LENGTH_LONG).show();
-
-            }
-
-        });
     }
 
     @Override
